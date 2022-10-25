f92d88682db575aa2710a59bbfba865df22dfce8
Vepp1 <vinipontarolla@gmail.com>
2022-10-25 13:45:50 +0000

    Add summernote to POSTS

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdm(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
